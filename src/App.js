fb30d791f0d80578ed2b7372669e748f6bd1c692
jawhnypoh <po.johnny@gmail.com>
2019-11-14 23:19:13 -0800

    Adding people api endpoint and more options to radios

--- src/App.js
+++ src/App.js
@@ -1,33 +1,92 @@
 import React, {Component} from 'react';
 import ResultsList from './components/resultsList';
+import { Radio } from '@material-ui/core';
 import Search from './components/search';
 import Starships from './apis/starships';
+import Vehicles from './apis/vehicles';
+import People from './apis/people';
 
 class App extends Component {
   render () {
     return (
       <div>
         <Search handleFormSubmit={this.handleSubmit} />
+        <center>
+        <div className="radios-container">
+          <Radio
+              value="starships"
+              checked={this.state.searchCategory === "starships"}
+              onChange={this.handleChange} /> Starships
+          <Radio
+              value="vehicles"
+              checked={this.state.searchCategory === "vehicles"}
+              onChange={this.handleChange} /> Vehicles  
+          <Radio
+              value="people"
+              checked={this.state.searchCategory === "people"}
+              onChange={this.handleChange} /> People 
+          <Radio
+              value="species"
+              checked={this.state.searchCategory === "species"}
+              onChange={this.handleChange} /> Species   
+          <Radio
+              value="planets"
+              checked={this.state.searchCategory === "planets"}
+              onChange={this.handleChange} /> Planets        
+        </div>
+        </center>
         <ResultsList resultsList={this.state.resultsList} />
       </div>
     );
   }
 
   state = {
-    resultsList: []
+    resultsList: [],
+    searchCategory: ''
+  };
+
+  handleChange = (event) => {
+    this.setState({
+        searchCategory: event.target.value
+    });
+    console.log(event.target.value);
   };
 
   handleSubmit = async (searchBarQuery) => {
-    const response = await Starships.get("/?", {
-      params: {
-        search: searchBarQuery
-      }
-    })
-
-    console.log("response data:", response.data)
-    this.setState({
-      resultsList: response.data.results
-    })
+    console.log("searchCategory: ", this.state.searchCategory);
+    if(this.state.searchCategory === "starships") {
+      const response = await Starships.get("/?", {
+        params: {
+          search: searchBarQuery
+        }
+      })
+      console.log("response data:", response.data)
+      this.setState({
+        resultsList: response.data.results
+      })
+    }
+    else if(this.state.searchCategory === "vehicles") {
+      const response = await Vehicles.get("/?", {
+        params: {
+          search: searchBarQuery
+        }
+      })
+      console.log("response data:", response.data)
+      this.setState({
+        resultsList: response.data.results
+      })
+    }
+    else if(this.state.searchCategory === "people") {
+      const response = await People.get("/?", {
+        params: {
+          search: searchBarQuery
+        }
+      })
+      console.log("response data:", response.data)
+      this.setState({
+        resultsList: response.data.results
+      })
+    }
   }
 }
 
--- src/components/search.js
+++ src/components/search.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import { Paper, InputBase, Radio } from '@material-ui/core';
+import { Paper, InputBase } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
 class Search extends Component {
     state = {
         searchCategory: ''
-    };
-
-    handleChange = (event) => {
-        this.setState({
-            searchCategory: event.target.value
-        });
-        console.log(event.target.value);
     };
 
     handleSubmit = event => {
@@ -37,28 +30,6 @@ class Search extends Component {
                                 type="text" value={this.state.query} />
                         </Paper>
                     </form>
-                    <div className="radios-container">
-                        <Radio
-                            value="starships"
-                            checked={this.state.searchCategory === "starships"}
-                            onChange={this.handleChange} /> Starships
-                        <Radio
-                            value="vehicles"
-                            checked={this.state.searchCategory === "vehicles"}
-                            onChange={this.handleChange} /> Vehicles  
-                        <Radio
-                            value="people"
-                            checked={this.state.searchCategory === "people"}
-                            onChange={this.handleChange} /> People 
-                        <Radio
-                            value="species"
-                            checked={this.state.searchCategory === "species"}
-                            onChange={this.handleChange} /> Species   
-                        <Radio
-                            value="planets"
-                            checked={this.state.searchCategory === "planets"}
-                            onChange={this.handleChange} /> Planets        
-                    </div>
                 </center>
             </div>
         )
